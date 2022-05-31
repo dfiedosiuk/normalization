bfef95b5d398e6053b778ad92299b05cb942137e
Daniel <d.fiedosiuk@adform.com>
2022-05-31 18:49:07 +0200

    init commit

--- src/main/scala/Normalization.scala
+++ src/main/scala/Normalization.scala
@@ -1,26 +1,39 @@
+import java.io.FileWriter
 import scala.io.Source
 
 object Normalization extends App {
 
-  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
-    val p = new java.io.PrintWriter(f)
-    try { op(p) } finally { p.close() }
-  }
+  val filepathResult = "result.txt"
+  val fw = new FileWriter(filepathResult, true)
 
-  def thruFile(filepath: String)={
-    val lines : Iterator[String] = Source.fromFile("data.csv").getLines()
+  val filepathIn = "data.txt"
+  val (max, min) = giveMinMax(filepathIn)
 
-    (0 until iter.length).foldLeft("start"){(m,n)=>
-      val next = iter.next
-      val x = next.split(",").last.toDouble
+  thruFile(filepathIn, max, min)
+  fw.close()
+
+
+  def thruFile(filepath: String, max: Double, min: Double)={
+    val linesThruLenght = Source.fromFile(filepath).getLines().length
+    val linesThru = Source.fromFile(filepath).getLines()
+    (0 until linesThruLenght).foldLeft(0){(m,n)=>
+      val x = linesThru.next.split(",").last.toDouble
+      val resultNormalization = normalizeValue(x, max, min)
+      val resultLine = s"${n},${resultNormalization}\n"
+      println(resultLine)
+      try{
+        fw.write(resultLine)
+      }
+      n
     }
   }
 
-  def giveMinMax(): (Double, Double) ={
-//    val lines : Iterator[String] = Source.fromFile("data.csv").getLines()
-    val lines = Iterator("1,200.0","1,300.0","1,2.0","1,1000.0")
+  def giveMinMax(fileIn: String): (Double, Double) ={
+    val linesLenght  = Source.fromFile(fileIn).getLines().length
+    val lines  = Source.fromFile(fileIn).getLines()
+//    val lines = Iterator("1,200.0","1,300.0","1,2.0","1,1000.0")
     val start = lines.next.split(",").last.toDouble
-    (0 until lines.length).foldLeft((start,start)){(minmax,z)=>
+    (0 until (linesLenght-1)).foldLeft((start,start)){ (minmax, z)=>
       val next = lines.next.split(",").last.toDouble
       next match {
         case n if n > minmax._2 => (minmax._1, n)
@@ -30,28 +43,10 @@ object Normalization extends App {
     }
   }
 
-  println(giveMinMax())
-
-//  def giveMin(iter: Iterator[Double]): Double ={
-//    val lines : Iterator[String] = Source.fromFile("data.csv").getLines()
-//    val start = lines.next.split(",").last.toDouble
-//    (0 until iter.length).foldLeft(start){(m,n)=>
-//      val next = lines.next.split(",").last.toDouble
-//      if (next < m ){
-//        next
-//      } else m
-//    }
-//  }
-
-
-
-//  val max = Source.fromFile("data.csv").getLines().flatMap(line => line.split(","))
-//  val min =
-//
-//  val words = Source.fromFile("data.csv").getLines().flatMap(line => line.split(","))
-////    .map(case (id, number) => )
-//
-//  def normalization()
-
+  def normalizeValue(x: Double, max: Double, min: Double): Double={
+    val up = x-min
+    val down = max-min
+    (up/down)*2-1
+  }
 
 }
